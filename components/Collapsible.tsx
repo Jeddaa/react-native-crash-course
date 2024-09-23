7154fc3c58b31bea2ce97dc4d304a1e6749a6de6
fatiha <fathiato@gmail.com>
2024-09-23 22:46:22 +0100

    setting up assets

--- components/Collapsible.tsx
+++ components/Collapsible.tsx
@@ -4,9 +4,12 @@ import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Colors } from '@/constants/Colors';
+import { Colors } from '@/old_constants/Colors';
 
-export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
+export function Collapsible({
+  children,
+  title
+}: PropsWithChildren & { title: string }) {
   const [isOpen, setIsOpen] = useState(false);
   const theme = useColorScheme() ?? 'light';
 
@@ -32,10 +35,10 @@ const styles = StyleSheet.create({
   heading: {
     flexDirection: 'row',
     alignItems: 'center',
-    gap: 6,
+    gap: 6
   },
   content: {
     marginTop: 6,
-    marginLeft: 24,
-  },
+    marginLeft: 24
+  }
 });
